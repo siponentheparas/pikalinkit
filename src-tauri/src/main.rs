b102785ddad47654fc84afc554e45f0b9b5d9b4a
siponen <osilvenn@gmail.com>
2023-05-16 22:26:54 +0300

    prepare for project start

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,13 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_internet() -> bool {
+    online::check(Some(2)).is_ok()
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_internet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/lib/links.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<main>
+    <div>
+    
+    </div>
+</main>
